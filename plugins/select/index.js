9b91b63428812ad613be4840b7a5f7a68e6ca7a6
Hackerbot1488 <gromovartem04@mail.ru>
2020-08-08 16:24:49 +0300

    added select plugin

---
+++ plugins/select/index.js
@@ -0,0 +1,25 @@
+import { Select } from './Select/select'
+import './styles/style.scss'
+const select = new Select('#select', {
+  placeholder: 'Select an element',
+  selectedId: '2',
+  onSelect(item) {
+    console.log(item)
+  },
+  data: [
+    {
+      id: '1', value: 'React'
+    },
+    {
+      id: '2', value: 'Angular'
+    },
+    {
+      id: '3', value: 'Vue'
+    },
+    {
+      id: '4', value: 'Ember'
+    }
+  ]
+})
+
+window.s = select
